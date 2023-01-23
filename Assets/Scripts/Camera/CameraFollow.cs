5fa64a4d57c3658ec69565eff201627f81522c32
KOstenkoY <zhenya.kostenko.2003@gmail.com>
2023-01-23 14:35:29 +0200

    Add script CameraFollow and state machine with any states

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // our player
+    [SerializeField] private Transform _target;
+
+    // must be quals how in the main player
+    [SerializeField] private float _constantSpeed = 3f;
+    // it's additional speed, that add to constant speed when the player speeds up
+    [SerializeField] private float _accelerationValue = 1.5f;
+
+    [SerializeField] private Transform _centerLimitPosition;
+
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+
+        transform.position = new Vector3(_target.position.x, 0, transform.position.z);
+
+        float width = _camera.pixelWidth / 2;
+    }
+
+    private void Update()
+    {
+        if(_target != null)
+        {
+            if(_target.position.x < _centerLimitPosition.position.x)
+            {
+                transform.Translate(_constantSpeed * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                Vector3 targetPosition = new Vector3(_target.position.x, transform.position.y, -10);
+
+                Vector3 pos = Vector3.Lerp(this.transform.position, targetPosition, _accelerationValue * Time.deltaTime);
+
+                transform.position = pos;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSlidingState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSlidingState : PlayerBaseState
+{
+    public override void EnterState(PlayerStateManager player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateState(PlayerStateManager player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
