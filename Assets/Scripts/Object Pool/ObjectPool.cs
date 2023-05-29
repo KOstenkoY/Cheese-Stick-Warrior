b888b80ddfccd57a4e5f49b4048a2f43f72ce986
KOstenkoY <zhenya.kostenko.2003@gmail.com>
2023-05-29 21:53:12 +0300

    Add Object Pool class.

---
+++ Assets/Scripts/Object Pool/ObjectPool.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private Poolable _poolablePrefab;
+
+    [SerializeField] private int _defaultCapacity = 15;
+    [SerializeField] private int _maxPoolSize = 30;
+
+    public IObjectPool<GameObject> Pool { get; private set; }
+
+    private void Awake()
+    {
+        Pool = new ObjectPool<GameObject>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, 
+            true, _defaultCapacity, _maxPoolSize);
+    }
+
+    private GameObject CreatePooledItem()
+    {
+        GameObject obj = Instantiate(_poolablePrefab.gameObject);
+        obj.GetComponent<Poolable>().Pool = Pool;
+
+        return obj;
+    }
+
+    private void OnTakeFromPool(GameObject obj) => obj.SetActive(true);
+
+    private void OnReturnedToPool(GameObject obj) => obj.SetActive(false);
+
+    private void OnDestroyPoolObject(GameObject obj) => Destroy(obj);
+}
